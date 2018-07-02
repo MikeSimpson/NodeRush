7fba89a696e979e0cced5cfe8e2972906ccd5a5e
mikeSimpson <mike.simpson@orbitremit.com>
2018-07-02 17:53:19 +1200

    Sorting direction was right but it was sorting as stirngs

--- models/highscore.js
+++ models/highscore.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var scoreSchema = new mongoose.Schema({
         name: String,
-        score: String
+        score: Number
     },
     {collection: 'high_scores'}
 );
