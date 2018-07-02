cab3bf99cd123574bf4c6489f22d12e569aefb22
mikeSimpson <mike.simpson@orbitremit.com>
2018-07-02 17:56:32 +1200

    Changed number of highscores displayed to 20

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res) {
         highscore
             .find({})
             .sort({ score: -1 })
-            .limit(10)
+            .limit(20)
             .exec()
     ]).then(result => {
         res.render('index', { title: "Bull Rush", scores: result[0] });
