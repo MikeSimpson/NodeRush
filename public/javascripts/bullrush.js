0d11e440d8c7daad14c8cee6dbab6e037e25e389
mikeSimpson <mike.simpson@orbitremit.com>
2018-08-23 13:14:53 +1200

    fixed bug and improved mobile

--- public/javascripts/bullrush.js
+++ public/javascripts/bullrush.js
@@ -965,7 +965,7 @@ class Sheep extends Actor {
         if (this.rooted) {
             return '#bfab92'
         }
-        if (powerKey && (game.players[0].pos.adjacent(this.pos) || game.players[1].pos.adjacent(this.pos))) {
+        if (powerKey && (game.players[0].pos.adjacent(this.pos) || (game.players.length > 1 && game.players[1].pos.adjacent(this.pos)))) {
             return this.powerUp.getColor()
         }
         return this.color
